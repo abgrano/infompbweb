945a4c0d09524e54e10af8d526734d9f93671374
Abg Ran <rano.paimin@gmail.com>
2024-01-15 17:10:27 +0800

    Update UserResource.php

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -131,6 +131,7 @@ class UserResource extends Resource
     public static function getPages(): array
     {
         return [
+
             'index' => Pages\ListUsers::route('/'),
             'create' => Pages\CreateUser::route('/create'),
             // 'view' => Pages\ViewUser::route('/{record}'),
